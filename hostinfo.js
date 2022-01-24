7e2f6f5e49c9f70ec5abeb53caddcea1bace07ca
Stef van Hooijdonk <stef.vanhooijdonk@coolblue.nl>
2022-01-24 11:18:32 +0100

    remove ambiguity app-appcontext

--- hostinfo.js
+++ hostinfo.js
@@ -1,14 +1,14 @@
 // to reuse later, and keep our adapters as stupid as possible
 class HostInfo {
 
-    constructor(app){
+    constructor(appcontext){
         
         const os = require( 'os');
 
         this.hostname = os.hostname();
         this.isMacOSX = os.platform() == "darwin";
         this.isWindows = os.platform() == "win32";
-        this.userDataPath = app.getPath('userData');
+        this.userDataPath = appcontext.getPath('userData');
         //this.applicationLogsPath = app.getPath('logs');
 
         // only on windows is this value relevant or available
